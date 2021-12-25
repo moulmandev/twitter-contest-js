ec69d7f9afd635f6abea91d7520ffebb6474d150
Théo "Moulman" Escolano <escolano725@gmail.com>
2021-12-25 18:38:17 +0100

    Initial files & gitignore

---
+++ config-exemple.js
@@ -0,0 +1,11 @@
+// Don't forget to rename this file to config.js
+
+const config = {
+    api: {
+        url: "https://api.twitter.com/2",
+        auth: {
+            url: "https://api.twitter.com/oauth2/token",
+            bearer: "yourBearerToken",
+        },
+    },
+}
---
+++ index.js
@@ -0,0 +1,4 @@
+const config = require("./config");
+
+console.log("Hello World, your auth bearer token is " + config.api.auth.bearer);
+
